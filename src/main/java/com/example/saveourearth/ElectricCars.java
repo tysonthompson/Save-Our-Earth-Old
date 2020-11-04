0547c7f1bcce8e6ba57fa3084f51d380140ec783
tysonthompson <56133772+tysonthompson@users.noreply.github.com>
2020-11-04 16:56:27 -0500

    Add files via upload

---
+++ src/main/java/com/example/saveourearth/ElectricCars.java
@@ -0,0 +1,14 @@
+package com.example.saveourearth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElectricCars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electric_cars);
+    }
+}
